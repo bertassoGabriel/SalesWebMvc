f0175bf0a2b5c0a947d3c6932037e0b010b654f3
bertassoGabriel <gabrieljacintho868@hotmail.com>
2020-05-26 09:47:57 -0400

    Seeding Service

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Data
@@ -16,6 +12,6 @@ namespace SalesWebMvc.Data
 
         public DbSet<Department> Department { get; set; }
         public DbSet<Seller> Seller { get; set; }
-        public DbSet<SalesRecord> SalesRecords { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
